5a8ac4be52244cba1e49c730333a8d6a77edbeba
carolinekle <caroline.klewinowski@gmail.com>
2023-04-17 21:41:38 -0400

    Nothing crazy

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const feature = document.querySelector(".feature"); 
 const tile = document.querySelectorAll(".tile");
+
+tile.addEventListener('mousehover', () =>{
+    feature.parentElement("img");
+    
+})
