4fd704647ee65591c7fce5112d94a7b82b1d2a10
carolinekle <caroline.klewinowski@gmail.com>
2023-06-26 15:50:11 -0400

    delete old files

--- script.js
+++
@@ -1,7 +0,0 @@
-const feature = document.querySelector(".feature"); 
-const tile = document.querySelectorAll(".tile");
-
-tile.addEventListener('mousehover', () =>{
-    feature.parentElement("img");
-    
-})
